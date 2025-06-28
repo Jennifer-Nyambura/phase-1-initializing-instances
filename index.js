1d57d7806b411649a04346b227d1501998ba0480
Jennifer <jenninyambu@gmail.com>
2025-06-28 10:40:26 +0300

    all changes

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here
+
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.dessert = dessert;
+  }
+}
+
+// Export classes for testing
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { Breakfast, Lunch, Dinner };
+}
